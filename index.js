7d2fc4c42271d1bca6a956a2a4cd3982b3eb5813
mmulahvictor <mmulahvictor@gmail.com>
2022-06-10 07:36:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(source, sort) {
+    return source.filter(
+      possibleMatch => possibleMatch.toLowerCase() === sort.toLowerCase()
+    );
+  }
+  const fuzzyMatch = (source, testString) => {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  const matchName = (source, sortName) => {
+    return source.filter((record) => record.name === sortName);
+  }
